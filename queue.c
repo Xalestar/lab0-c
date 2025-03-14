6c9c0ea34e2ed50c3bf616e975d9f795563a356d
alexlin066 <alex.h.r.lin@gmail.com>
2025-03-14 10:49:27 +0800

    Free All Allocated Memory in Queue
    Ensure all dynamically allocated memory in the queue is properly freed.
    Previously, the queue head itself was not freed, leading to a memory
    leak. This update guarantees that both the elements and the head are
    deallocated, preventing potential resource leaks.
    
    Change-Id: I567cc1c9062eb8f340d0d2916202cff017f43db5

--- queue.c
+++ queue.c
@@ -23,7 +23,20 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *current, *next;
+    list_for_each_safe (current, next, head) {
+        element_t *entry = list_entry(current, element_t, list);
+        free(entry->value);
+        free(entry);
+        // list_del(current); doesn't need
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
